5ffc4f67262bda5a4c87ca09613d2b1e47a9c8a5
Leon Franco <leonard.k.franco@gmail.com>
2019-10-30 16:23:27 -0700

    Added logic to logout button

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,2 +1,5 @@
 // for register page
 $("#registerButton").click(() => window.location.href = `${window.location.origin}/register`);
+
+//for main menu page
+$("#logoutButton").click(() => window.location.href = `${window.location.origin}/logout`);
