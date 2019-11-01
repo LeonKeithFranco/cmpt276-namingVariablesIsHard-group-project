1a944c12e45989fc7ffc491614dbca6dca8d0e4a
Leon Franco <leonard.k.franco@gmail.com>
2019-11-01 11:13:21 -0700

    Updated login and registeration logic to reject incorrect inputs

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -3,16 +3,21 @@ let selector = '#loginForm';
 $(selector).submit((event) => {
   event.preventDefault(); // prevents page from refreshing when submitting form
 
-  $.ajax({
-    type: 'POST',
-    url: '/login',
-    data: $(selector).serialize(),
-    success: (response) => {
+  if (!hasIncorrectInputs()) {
+    $.ajax({
+      type: 'POST',
+      url: '/login',
+      data: $(selector).serialize(),
+      success: (response) => {
         window.location.href = window.location.href = `${window.location.origin}/main-menu`;
-    },
-    error: (jqXHR, textStatus, errorThrown) => {
-      console.log(`Error: ${textStatus} - ${errorThrown}`);
-      alert(`${errorThrown}`);
-    }
-  });
+      },
+      error: (jqXHR, textStatus, errorThrown) => {
+        console.log(`Error: ${textStatus} - ${errorThrown}`);
+        alert(`${errorThrown}`);
+      }
+    });
+  }
+  else {
+    alert('Please don\'t leave any fields blank and don\'t use any spaces in your username and password');
+  }
 });
--- public/scripts/register.js
+++ public/scripts/register.js
@@ -3,17 +3,22 @@ let selector = '#registerForm';
 $(selector).submit((event) => {
   event.preventDefault(); // prevents page from refreshing when submitting form
 
-  $.ajax({
-    type: 'POST',
-    url: '/register',
-    data: $(selector).serialize(),
-    success: (response) => {
-      alert('Registration successful\nWelcome to Scribbltaire.io!');
-      window.location.href = window.location.href = `${window.location.origin}/login`;
-    },
-    error: (jqXHR, textStatus, errorThrown) => {
-      console.log(`Error: ${textStatus} - ${errorThrown}`);
-      alert(`${errorThrown}`);
-    }
-  });
+  if (!hasIncorrectInputs()) {
+    $.ajax({
+      type: 'POST',
+      url: '/register',
+      data: $(selector).serialize(),
+      success: (response) => {
+        alert('Registration successful\nWelcome to Scribbltaire.io!');
+        window.location.href = window.location.href = `${window.location.origin}/login`;
+      },
+      error: (jqXHR, textStatus, errorThrown) => {
+        console.log(`Error: ${textStatus} - ${errorThrown}`);
+        alert(`${errorThrown}`);
+      }
+    });
+  }
+  else {
+    alert('Please don\'t leave any fields blank and don\'t use any spaces in your username and password');
+  }
 });
