5c4f5ac44bbb41d41167d7a8dfc14512b446f32f
Leon Franco <leonard.k.franco@gmail.com>
2019-11-04 20:45:12 -0800

    Modularizes register route

---
+++ server/routes/register-route.js
@@ -0,0 +1,67 @@
+const registerRoute = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const saltRounds = 8; // for hashing; the higher the number, the more secure the hash
+
+registerRoute.get('/', (req, res) => {
+  console.log('Landed on register page');
+
+  res.render('pages/register');
+});
+registerRoute.post('/', (req, res) => {
+  console.log('User register requested');
+
+  const { username, password, passwordReconfirm } = req.body;
+  let registerQuery = `SELECT * FROM Users WHERE username=\'${username}\'`
+
+  req.pool.query(registerQuery, (error, result) => {
+    if (error) {
+      console.error(error);
+
+      res.send(error);
+    }
+
+    const registerResponse = (httpResponseCode, msg) => {
+      console.log(msg);
+
+      res.statusMessage = msg;
+      res.status(httpResponseCode).end();
+    }
+
+    switch (result.rows.length) {
+      case 0: // no existing user in db
+        if (password === passwordReconfirm) { // password match
+          bcrypt.hash(password, saltRounds, (error, hashedPassword) => {
+            if (error) {
+              console.error(error);
+
+              res.send(error);
+            }
+
+            registerQuery = `INSERT INTO Users(username,password) VALUES (\'${username}\',\'${hashedPassword}\')`;
+
+            req.pool.query(registerQuery, (error, result) => {
+              if (error) {
+                console.error(error);
+
+                res.send(error);
+              }
+
+              registerResponse(req.httpStatus.CREATED, 'New user added to database');
+            });
+          });
+        }
+        else { // password mismatch
+          registerResponse(req.httpStatus.CONFLICT, 'Passwords do not match');
+        }
+        break;
+      case 1: // existing user in db
+        registerResponse(req.httpStatus.CONFLICT, 'User already exists');
+        break;
+      default:
+        throw new Error('Non-unique user in database');
+    }
+  });
+});
+
+module.exports = registerRoute;
--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ const bcrypt = require('bcryptjs');
 
 const indexRoute = require('./routes/index-route');
 const loginRoute = require('./routes/login-route');
+const registerRoute = require('./routes/register-route');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-const saltRounds = 8; // for hashing; the higher the number, the more secure the hash
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
@@ -39,67 +39,7 @@ app.set('view engine', 'ejs');
 
 app.use('/', indexRoute);
 app.use('/login', loginRoute);
-
-app.get('/register', (req, res) => {
-  console.log('Landed on register page');
-
-  res.render('pages/register');
-});
-app.post('/register', (req, res) => {
-  console.log('User register requested');
-
-  const { username, password, passwordReconfirm } = req.body;
-  let registerQuery = `SELECT * FROM Users WHERE username=\'${username}\'`
-
-  req.pool.query(registerQuery, (error, result) => {
-    if (error) {
-      console.error(error);
-
-      res.send(error);
-    }
-
-    const registerResponse = (httpResponseCode, msg) => {
-      console.log(msg);
-
-      res.statusMessage = msg;
-      res.status(httpResponseCode).end();
-    }
-
-    switch (result.rows.length) {
-      case 0: // no existing user in db
-        if (password === passwordReconfirm) { // password match
-          bcrypt.hash(password, saltRounds, (error, hashedPassword) => {
-            if (error) {
-              console.error(error);
-
-              res.send(error);
-            }
-
-            registerQuery = `INSERT INTO Users(username,password) VALUES (\'${username}\',\'${hashedPassword}\')`;
-
-            req.pool.query(registerQuery, (error, result) => {
-              if (error) {
-                console.error(error);
-
-                res.send(error);
-              }
-
-              registerResponse(req.httpStatus.CREATED, 'New user added to database');
-            });
-          });
-        }
-        else { // password mismatch
-          registerResponse(req.httpStatus.CONFLICT, 'Passwords do not match');
-        }
-        break;
-      case 1: // existing user in db
-        registerResponse(req.httpStatus.CONFLICT, 'User already exists');
-        break;
-      default:
-        throw new Error('Non-unique user in database');
-    }
-  });
-});
+app.use('/register', registerRoute);
 
 app.get('/main-menu', (req, res) => {
   const sesh = req.session;
