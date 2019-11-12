0b91af726f1a7ef6f09d1bde3c3ccc0fa1b2072a
Leon Franco <leonard.k.franco@gmail.com>
2019-11-11 21:20:35 -0800

    Refactors send-drawing route

--- server/routes/send-drawing-route.js
+++ server/routes/send-drawing-route.js
@@ -3,16 +3,16 @@ const quickdraw = require('../lib/quickdraw/quickdraw-api');
 const qdsr = require('quickdraw-svg-render')
 
 sendDrawingRoute.get('/random', (req, res) => {
-    console.log('Random drawing requested');
+  console.log('Random drawing requested');
 
-    quickdraw.getRandomDrawing((drawing) => {
-        const svgArray = qdsr(drawing.drawing, true);
-        const svgHTMLElem = svgArray.reduce((currentVal, nextVal) => {
-            return currentVal + nextVal;
-        });
-    
-        res.status(200).send({ word: drawing.word, svg: svgHTMLElem });
-      });
+  quickdraw.getRandomDrawing((drawing) => {
+    const svgArray = qdsr(drawing.drawing, true);
+    const svgHTMLElem = svgArray.reduce((currentVal, nextVal) => {
+      return currentVal + nextVal;
+    });
+
+    res.status(req.httpStatus.OK).send({ word: drawing.word, svg: svgHTMLElem });
+  });
 });
 
 module.exports = sendDrawingRoute;
