96dc9c2b1c64c31044305c2bd025607d15c5be96
Alex Zhao <alex_zhao@sfu.ca>
2019-11-12 18:40:43 -0800

    Added following functions: 1) to get category name by index number (0-344) 2) to get category size by category name 3) to get a specific quick draw drawing by category name and id number

--- server/lib/quickdraw/quickdraw-api.js
+++ server/lib/quickdraw/quickdraw-api.js
@@ -15,11 +15,59 @@ module.exports = {
     const category = categories[categoryIndex];
     const URL = `https://quickdrawfiles.appspot.com/drawing/${category}?&key=${API_KEY}&isAnimated=false&format=json`;
 
+    //debugging info
+    console.log(`Category: ${category}`);
+    console.log(`Category length: ${categories.length}`);
+    console.log(`Category index: ${categoryIndex}`);
+    console.log(`Category: ${module.exports.getCategory(categoryIndex)}`);
+    module.exports.getCategorySize(category, (size) => {console.log(`Category size: ${size}`);});
+
     request(URL, (error, response, body) => {
       if (error) {
         throw error;
       }
 
+      setTimeout(callback, 0, JSON.parse(body));
+    });
+  },
+
+  /*
+    Pre-condition: Requires a callback as input
+    Post-condition: Will get a specific Quick, Draw! drawing from Google's web API, convert the returned drawing into a
+                    JSON object, then apply the callback to the drawing JSON object 
+  */
+  getDrawing: function (category, id, callback) {
+    const URL = `https://quickdrawfiles.appspot.com/drawing/${category}?id=${id}&key=${API_KEY}&isAnimated=false&format=json`;
+
+    request(URL, (error, response, body) => {
+      if (error) {
+        throw error;
+      }
+
+      setTimeout(callback, 0, JSON.parse(body));
+    });
+  },
+
+  /*
+    Pre-condition: Requires an integer between 0 and 344 (inclusive) as input
+    Post-condition: Will return a category name from the corresponding given index
+  */
+  getCategory: function (index) {
+    if (index >= 0 && index < categories.length) return categories[index];
+  },
+
+  /*
+    Pre-condition: Requires a category name as a string and a callback as input
+    Post-condition: Will get the number of random Quick, Draw! drawings of the given category from Google's web API, convert the returned number into a JSON object, then apply the callback to the number
+    Additional notes: used to obtain an upper bound for generating random drawing indexes
+  */
+  getCategorySize: function (category, callback) {
+    const URL = `https://quickdrawfiles.appspot.com/drawing/${category}/count?key=${API_KEY}`;
+
+    request(URL, (error, response, body) => {
+      if (error) {
+        throw error;
+      }
       setTimeout(callback, 0, JSON.parse(body));
     });
   }
