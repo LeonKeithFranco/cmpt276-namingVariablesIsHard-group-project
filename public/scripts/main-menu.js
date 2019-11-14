ecd8ee580ced3e09d9ae9fbaf60ef792cdc6fc81
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 13:30:07 -0800

    Adds logic for game mode buttons

--- public/scripts/main-menu.js
+++ public/scripts/main-menu.js
@@ -10,3 +10,26 @@ $(document).ready(() => {
 const standardBtn = $('#standardButton');
 const oddOneOutBtn = $('#oddOneOutButton');
 const timedBtn = $('#timedButton');
+
+let gameMode;
+let difficulty;
+
+function gameModePicked() {
+    gameModeMenu.hide();
+    difficultyMenu.show();
+}
+
+standardBtn.click(function() {
+    gameMode = standardBtn.text();
+    gameModePicked()
+});
+
+oddOneOutBtn.click(function() {
+    gameMode = oddOneOutBtn.text();
+    gameModePicked()
+});
+
+timedBtn.click(function() {
+    gameMode = timedBtn.text();
+    gameModePicked()
+});
