e1fbadc2acdb4395f8791f99f29670906c56c06f
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 15:13:36 -0800

    Creates standard game mode ejs

--- server/routes/game-mode-route.js
+++ server/routes/game-mode-route.js
@@ -4,9 +4,8 @@ gameModeRoute.get('/Standard/:difficulty', (req, res) => {
     const difficulty = req.params.difficulty;
 
     console.log(`${req.session.user} started a Standard game with ${difficulty} difficulty`);
-
-    res.send({ difficulty: difficulty });
-    // res.render('pages/standard', { difficulty: difficulty });
+    
+    res.render('pages/standard', { difficulty: difficulty });
 });
 
 module.exports = gameModeRoute;
