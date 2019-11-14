c6a43a40d27fd8888e3b399d2ace516eec74e953
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 13:43:18 -0800

    Adds logic for difficulty buttons

--- public/scripts/main-menu.js
+++ public/scripts/main-menu.js
@@ -7,9 +7,9 @@ $(document).ready(() => {
     confirmationMenu.hide();
 });
 
+
 let gameMode;
 let difficulty;
-
 
 const standardBtn = $('#standardButton');
 const oddOneOutBtn = $('#oddOneOutButton');
@@ -40,3 +40,34 @@ const easyBtn = $('#easyButton');
 const normalBtn = $('#normalButton');
 const hardBtn = $('#hardButton');
 const backBtn = $('#backButton');
+
+function difficultyPicked() {
+    difficultyMenu.hide();
+    confirmationMenu.show();
+}
+
+easyBtn.click(() => {
+    difficulty = easyBtn.text();
+    difficultyPicked();
+});
+
+normalBtn.click(() => {
+    difficulty = normalBtn.text();
+    difficultyPicked();
+});
+
+hardBtn.click(() => {
+    difficulty = hardBtn.text();
+    difficultyPicked();
+});
+
+function reset() {
+    gameMode = undefined;
+    difficulty = undefined;
+
+    difficultyMenu.hide();
+    confirmationMenu.hide();
+    gameModeMenu.show();
+}
+
+backBtn.click(reset);
