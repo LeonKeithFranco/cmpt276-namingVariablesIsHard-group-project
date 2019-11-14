d111101afb2230407980b63ceee9b90eb1dabae6
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 14:07:31 -0800

    Sets up standard game mode route for testing

--- server/routes/game-mode-route.js
+++ server/routes/game-mode-route.js
@@ -5,7 +5,8 @@ gameModeRoute.get('/Standard/:difficulty', (req, res) => {
 
     console.log(`${req.session.user} started a Standard game with ${difficulty} difficulty`);
 
-    res.render('pages/standard', { difficulty: difficulty });
+    res.send({ difficulty: difficulty });
+    // res.render('pages/standard', { difficulty: difficulty });
 });
 
 module.exports = gameModeRoute;
