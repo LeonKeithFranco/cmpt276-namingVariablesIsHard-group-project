ff1fece9b81f2aef960bcb7d500ad37287005fcf
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 13:08:42 -0800

    Refactors main menu js

--- public/scripts/main-menu.js
+++ public/scripts/main-menu.js
@@ -1,33 +1,7 @@
-const display = $('#display');
-let defaultDisplayHtml;
-
 $(document).ready(() => {
     defaultDisplayHtml = display.html();
 });
 
-const standardBtn = $("#standardButton");
-const oddOneOutBtn = $("#oddOneOutButton");
-const timedBtn = $("#timedButton");
-
-const difficultyHtml = `
-    <h4 class="text">Choose your difficulty</h4>
-    <button id="easyButton" class="button">Easy</button>
-    <br>
-    <button id="normalButton" class="button">Normal</button>
-    <br>
-    <button id="hardButton" class="button">Hard</button>
-    <br>
-    <button id="backButton" class="button">Back</button>
-`
-
-standardBtn.click(() => {
-    display.html(difficultyHtml);
-});
-
-oddOneOutBtn.click(() => {
-    display.html(difficultyHtml);
-});
-
-timedBtn.click(() => {
-    display.html(difficultyHtml);
-});
+const standardBtn = $('#standardButton');
+const oddOneOutBtn = $('#oddOneOutButton');
+const timedBtn = $('#timedButton');
