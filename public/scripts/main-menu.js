fcc8a9267eb9339f1edd180ddaea1b0a1c430b13
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 10:39:34 -0800

    User is now asked to choose a difficulty after clicking a game mode button

--- public/scripts/main-menu.js
+++ public/scripts/main-menu.js
@@ -1,3 +1,24 @@
 const standardBtn = $("#standardButton");
 const oddOneOutBtn = $("#oddOneOutButton");
 const timedBtn = $("#timedButton");
+
+const difficultyHtml = `
+    <h4 class="text">Choose your difficulty</h4>
+    <button id="easyButton" class="button">Easy</button>
+    <br>
+    <button id="normalButton" class="button">Normal</button>
+    <br>
+    <button id="hardButton" class="button">Hard</button>
+`
+
+standardBtn.click(() => {
+    $("#display").html(difficultyHtml);
+});
+
+oddOneOutBtn.click(() => {
+    $("#display").html(difficultyHtml);
+});
+
+timedBtn.click(() => {
+    $("#display").html(difficultyHtml);
+});
