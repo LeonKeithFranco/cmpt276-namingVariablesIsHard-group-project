3e89e4f3fb9919aa6af3c6cd283f7dd50d35d37a
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 16:41:46 -0800

    Adds redirect

--- server/routes/game-mode-route.js
+++ server/routes/game-mode-route.js
@@ -1,10 +1,15 @@
 const gameModeRoute = require('express').Router();
 
+gameModeRoute.get('/', (req, res) => {
+    console.log(`${req.session.user} redirected to login`);
+
+    res.redirect('/main-menu');
+});
 gameModeRoute.get('/standard/:difficulty', (req, res) => {
     const difficulty = req.params.difficulty;
 
     console.log(`${req.session.user} started a Standard game with ${difficulty} difficulty`);
-    
+
     res.render('pages/standard', { difficulty: difficulty });
 });
 
