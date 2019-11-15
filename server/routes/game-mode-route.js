7d97c29f6d702017a6262e62f7a7dde905abce96
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 16:43:32 -0800

    Adds other routes for the other game modes

--- server/routes/game-mode-route.js
+++ server/routes/game-mode-route.js
@@ -12,5 +12,19 @@ gameModeRoute.get('/standard/:difficulty', (req, res) => {
 
     res.render('pages/standard', { difficulty: difficulty });
 });
+gameModeRoute.get('/odd-one-out/:difficulty', (req, res) => {
+    const difficulty = req.params.difficulty;
+
+    console.log(`${req.session.user} started an Odd One Out game with ${difficulty} difficulty`);
+
+    res.render('pages/odd-one-out', { difficulty: difficulty });
+});
+gameModeRoute.get('/timed/:difficulty', (req, res) => {
+    const difficulty = req.params.difficulty;
+
+    console.log(`${req.session.user} started a Timed game with ${difficulty} difficulty`);
+
+    res.render('pages/timed', { difficulty: difficulty });
+});
 
 module.exports = gameModeRoute;
