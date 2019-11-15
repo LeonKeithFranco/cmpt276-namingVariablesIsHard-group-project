7e74e7cae21830043946ba9ca51df61100f0603f
Leon Franco <leonard.k.franco@gmail.com>
2019-11-14 16:16:14 -0800

    Adds logic for the back to main menu button

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,7 +3,10 @@ function goTo(page) {
 }
 
 // for register page
-$("#registerButton").click(goTo('register'));
+$('#registerButton').click(goTo('register'));
 
-//for main menu page
-$("#logoutButton").click(goTo('logout'));
+// for main menu page
+$('#logoutButton').click(goTo('logout'));
+
+// for standard game mode page
+$('#backToMainMenuButton').click(goTo('main-menu'));
