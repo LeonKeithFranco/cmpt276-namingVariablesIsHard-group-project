85f4578a82ef91a2d3ed9aa304328116f2613dce
Hyeonseob Yang <nick_yang@sfu.ca>
2019-11-17 10:46:38 -0800

    Added listeners to images

--- public/scripts/odd-one-out.js
+++ public/scripts/odd-one-out.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    console.log(x);
+    console.log(difficulty);
     let num;
     if (difficulty === 'easy') {
         num = 6;
@@ -7,5 +7,16 @@ window.onload = function() {
         num = 4;
     } else if (difficulty === 'hard') {
         num = 3;
+    }
+    console.log(num);
+
+    let i;
+    for (i = 1; i <= num; i++) {
+        let j = i;
+        console.log(j);
+        let tempString = 'drawing' + j;
+        document.getElementById(tempString).addEventListener('click', function () {
+            console.log(j);
+        });
     }
 };
