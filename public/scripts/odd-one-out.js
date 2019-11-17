4e2b4901e28a46b7e42ebd150baa8a438d0f3249
Hyeonseob Yang <nick_yang@sfu.ca>
2019-11-17 10:18:17 -0800

    Accessed difficulty in JavaScript

---
+++ public/scripts/odd-one-out.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    console.log(x);
+    let num;
+    if (difficulty === 'easy') {
+        num = 6;
+    } else if (difficulty === 'normal') {
+        num = 4;
+    } else if (difficulty === 'hard') {
+        num = 3;
+    }
+};
