1592ed05fa7ba3cd03089036e2441bfb3f1c0e3b
Hyeonseob Yang <nick_yang@sfu.ca>
2019-11-18 21:49:44 -0800

    Put standard game mode logic in timed mode

---
+++ public/scripts/timed.js
@@ -0,0 +1,105 @@
+const socket = io.connect(window.location.origin);
+
+const drawingDivs = $(".drawing");
+const input = $('#wordInput');
+const scoreDisplay = $('#score');
+const submitGuessBtn = $('#submitGuessButton');
+const playAgainBtn = $('#playAgainButton');
+const hintDisplay = $('#hint');
+
+let svgArr = [];
+let category = "";
+let drawingCount = 0;
+let playerScore = 0;
+let continueGame = true;
+let allDrawingsLoaded = false;
+
+socket.on('serverSendRandomCategoryName', (cat) => {
+    category = cat;
+    hintDisplay.text(`Hint: ${category.replace(/\S/g, "-")}`);
+    socket.emit('clientRequestCategorySize', category);
+});
+
+socket.on('serverSendCategorySize', (categorySize) => {
+    const drawingIds = randomArray(categorySize, drawingDivs.length);
+
+    drawingDivs.each(function (index) {
+        socket.emit('clientRequestDrawing', { category: category, id: drawingIds[index] });
+    });
+});
+
+socket.on('serverSendDrawing', (drawingData) => {
+    const { word, svg } = drawingData;
+    svgArr.push(svg);
+    $(drawingDivs[drawingCount]).html(svgArr[drawingCount]);
+    drawingCount++;
+    allDrawingsLoaded = drawingCount === drawingDivs.length;
+});
+
+function fillDrawingDivs() {
+    svgArr = [];
+    category = "";
+    drawingCount = 0;
+    allDrawingsLoaded = false;
+
+    socket.emit('clientRequestRandomCategoryName');
+}
+
+function randomArray(upperbound, size) {
+    let arr = [];
+    while (arr.length < size) {
+        let r = randomRange(upperbound);
+        if (arr.indexOf(r) === -1) arr.push(r);
+    }
+    return arr;
+};
+
+function randomRange(upperbound) {
+    return Math.floor(Math.random() * upperbound);
+};
+
+$(document).ready(() => {
+    scoreDisplay.text(`Score: ${playerScore}`);
+    fillDrawingDivs();
+});
+
+submitGuessBtn.click(() => {
+    const playerGuess = input.val().trim().toLowerCase();
+
+    if (continueGame && playerGuess) {
+        const answer = category.toLowerCase();
+
+        if (playerGuess === answer) {
+            scoreDisplay.text(`Score: ${++playerScore}`);
+            input.val('');
+
+            fillDrawingDivs();
+        } else {
+            continueGame = false;
+
+            alert(`Game over!\nThe word was "${category}".\n\nScore: ${playerScore}\n\nClick "Play Again" to start a new game!`);
+        }
+    }
+});
+
+input.keypress(function (e) {
+    let key = e.which;
+
+    if (key == 13) { // hitting enter key
+        submitGuessBtn.click();
+    }
+});
+
+playAgainBtn.click(() => {
+    if (allDrawingsLoaded) {
+        console.log('play again clicked')
+        continueGame = true;
+
+        playerScore = 0;
+
+        scoreDisplay.text(`Score: ${playerScore}`);
+        input.val('');
+
+        fillDrawingDivs();
+    }
+});
