b5e9b09b0655ffb9ea5f646aca394620ac7da86e
Leon Franco <leonard.k.franco@gmail.com>
2019-11-19 12:59:44 -0800

    Adds test for quickdraw getDrawing function

--- test/quickdraw-api.js
+++ test/quickdraw-api.js
@@ -3,7 +3,7 @@ const quickdraw = require('../server/lib/quickdraw/quickdraw-api');
 
 describe('quickdraw-api', () => {
   describe('getRandomDrawing()', () => {
-    it('Should return quickdraw JSON object', (asyncTestDone) => {
+    it('Should return a quickdraw JSON object', (asyncTestDone) => {
       quickdraw.getRandomDrawing((drawingObj) => {
         assert.isObject(drawingObj);
         assert.property(drawingObj, 'key_id');
@@ -16,6 +16,34 @@ describe('quickdraw-api', () => {
         asyncTestDone();
       });
     });
-    
   });
+
+  describe('getDrawing()', () => {
+    const category = 'cat'
+    const drawingId = 0;
+
+    it('Should return a quickdraw JSON object', (asyncTestDone) => {
+      quickdraw.getDrawing(category, drawingId, (drawingObj) => {
+        assert.isObject(drawingObj);
+        assert.property(drawingObj, 'key_id');
+        assert.property(drawingObj, 'word');
+        assert.property(drawingObj, 'recognized');
+        assert.property(drawingObj, 'timestamp');
+        assert.property(drawingObj, 'countrycode');
+        assert.property(drawingObj, 'drawing');
+
+        asyncTestDone();
+      });
+    });
+
+    it(`Should return a quickdraw drawing of category "${category}"`, (asyncTestDone) => {
+      quickdraw.getDrawing(category, drawingId, (drawingObj) => {
+        assert.propertyVal(drawingObj, 'word', category);
+  
+        asyncTestDone();
+      });
+    });
+  });
+
+
 });
