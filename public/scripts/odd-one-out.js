29a63513498bebdcbdfeae68dc17d5922ebafc99
Hyeonseob Yang <nick_yang@sfu.ca>
2019-11-24 20:58:21 -0800

    Added proper game over message and deleted old code

--- public/scripts/odd-one-out.js
+++ public/scripts/odd-one-out.js
@@ -17,117 +17,6 @@ let continueGame = true;
 let sameDrawingsLoaded = false;
 let allDrawingsLoaded = false;
 
-// window.onload = function() {
-//     for (let i = 1; i <= num; i++) {
-//         document.getElementById('drawing' + i).addEventListener('click', function () {
-//             select(i - 1);
-//         });
-//     }
-//     gameLoop();
-// };
-//
-// function gameLoop() {
-//     getRandomCategory(num);
-// }
-// function getRandomCategory(numPics) {
-//     socket.emit('clientRequestRandomCategoryName');
-//
-//     categoryTimer = setInterval(function() {
-//         getCategorySize(numPics);
-//     }, checkTime);
-// }
-//
-// function getCategorySize(numPics) {
-//     if (category === "") {
-//     } else {
-//         clearInterval(categoryTimer);
-//         socket.emit('clientRequestCategorySize', category);
-//         sizeTimer = setInterval(function() {
-//             getPics(numPics);
-//         }, checkTime);
-//     }
-//
-// }
-//
-// function getPics(numPics) {
-//     if (maxSize === 0) {
-//
-//     } else {
-//         clearInterval(sizeTimer);
-//         for (let i = 1; i <= numPics; i++) {
-//             requestDrawing();
-//         }
-//         drawTimer = setInterval(function() {
-//             getOdd();
-//         }, checkTime);
-//     }
-// }
-//
-// function requestDrawing() {
-//     socket.emit('clientRequestDrawing', {
-//         category: category,
-//         id: randomRange(maxSize)
-//     });
-// }
-//
-// function getOdd() {
-//     if (svgList[num-1] === undefined) {
-//
-//     } else if (svgList[num] === undefined && !oneIteration) {
-//         clearInterval(drawTimer);
-//         maxSize = 0;
-//         category = "";
-//         oneIteration = true;
-//
-//         getRandomCategory(1);
-//     }  else if (svgList[num] != undefined) {
-//         clearInterval(drawTimer);
-//         drawPictures();
-//     }
-// }
-//
-// function drawPictures() {
-//     odd = randomRange(num);
-//
-//     for (let i = 1; i <= num; i++) {
-//         document.getElementById("drawing" + i).innerHTML = svgList[i-1];
-//     }
-//     document.getElementById("drawing" + (odd+1)).innerHTML = svgList[num];
-//     loaded = true;
-// }
-//
-// function select (guess) {
-//     if (loaded) {
-//         if (guess == odd) {
-//             score++;
-//             document.getElementById('score').innerHTML = "Score: " + score;
-//             console.log('nice!');
-//         } else {
-//             gameOver();
-//         }
-//         while (svgList.length != 0) {
-//             svgList.pop();
-//         }
-//         maxSize = 0;
-//         category = "";
-//         oneIteration = false;
-//         clearInterval(sizeTimer);
-//         clearInterval(categoryTimer);
-//         clearInterval(drawTimer);
-//         if (svgList.length === 0) {
-//             gameLoop();
-//         }
-//         loaded = false;
-//     }
-// }
-//
-// function gameOver() {
-//     alert('Game over: it was picture ' + (odd + 1));
-//     score = 0;
-//     document.getElementById('score').innerHTML = "Score: " + score;
-// }
-
-
 socket.on('serverSendRandomCategoryName', (cat) => {
     if (category === "") {
         category = cat;
@@ -236,7 +125,7 @@ function select(index) {
         } else {
             continueGame = false;
 
-            alert(`Game over!\nThe word was "${category}".\n\nScore: ${playerScore}\n\nClick "Play Again" to start a new game!`);
+            alert(`Game over!\nIt was picture ${odd + 1}.\n\nScore: ${playerScore}\n\nClick "Play Again" to start a new game!`);
         }
     }
 };
