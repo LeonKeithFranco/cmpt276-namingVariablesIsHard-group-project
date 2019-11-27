d743456e6be015096140af68a69961ce235ee341
Eric Kozak <ericthehumanstudent@gmail.com>
2019-11-27 15:31:38 -0800

    Adds ability to use multiple pre-loaded drawings from database

--- server/server.js
+++ server/server.js
@@ -135,48 +135,53 @@ io.on('connection', (socket) => {
   socket.on('clientRequestCountFromCategory', (data) =>{
     const { category, count } = data;
     console.log(`${count} drawings requested for: ${category}`);
-    quickdraw.getCategorySize(category, (size) => {
-      console.log(`size of ${category} category: ${size}`);
-      return sendCountFromCategory(category, count, size);
-    });
+    return sendCountFromCategory(category, count);
   });
 
   socket.on('clientRequestFromCategory', (data) =>{
     const category = data;
     console.log(`single drawing requested for: ${category}`);
+    sendCountFromCategory(category, 1);
+  });
 
+  function sendCountFromCategory(category, count) {
     let drawingQuery = `
         DELETE FROM Preloaded_drawings
         WHERE id = ANY (ARRAY(
           SELECT id FROM Preloaded_drawings
           WHERE category = '${category}'
           AND recognized = TRUE
-          LIMIT 1))
+          LIMIT ${count}))
         RETURNING *;`;
     serverPool.query(drawingQuery, (error, result) => {
       if (error) {
         console.error(error);
       } else {
-        if(result.rows.length > 0) {
-          let parsedDrawing = JSON.parse(result.rows[0].drawing);
+
+        for(let i = 0; i < result.rows.length; i++) {
+          const parsedDrawing = JSON.parse(result.rows[i].drawing);
           quickdraw.convertDrawing(parsedDrawing, (convertedDrawing) => {
             socket.emit('serverSendDrawing', convertedDrawing);
           });
-        } else {
-          console.log(`no preloaded images in category ${category}, falling back to direct API call`);
+        }
+
+        if(result.rows.length < count) {
+          console.log(`insufficient preloaded images in category ${category}, falling back to direct API call`);
+
+          const remaining = count - result.rows.length;
+
           quickdraw.getCategorySize(category, (size) => {
-            console.log(`size of ${category} category: ${size}`);
-            return sendRandomFromCategory(category, size);
+            function getRemaining (category, remaining, size) {
+              for(let i = 0; i < remaining; i++) {
+                console.log(`making direct request for request for ${category}`);
+                sendRandomFromCategory(category, size);
+              }
+            }
+            return getRemaining(category, remaining, size);
           });
         }
       }
     });
-  });
-
-  function sendCountFromCategory(category, count, size) {
-    for(let i = 0; i < count; i++) {
-      sendRandomFromCategory(category, size);
-    }
   }
 
   function sendRandomFromCategory(category, size) {
