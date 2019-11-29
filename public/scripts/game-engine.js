3dc29f29bad03f33cc1dcc20fb629ae4aeb50a79
Leon Franco <leonard.k.franco@gmail.com>
2019-11-29 10:38:01 -0800

    Moves game logic from standard js to game-engine js

--- public/scripts/game-engine.js
+++ public/scripts/game-engine.js
@@ -1,3 +1,104 @@
-const gameEngine = (function(){
-	
+const gameEngine = (function () {
+  const socket = io.connect(window.location.origin);
+
+  const drawingDivs = $(".drawing");
+  const input = $('#wordInput');
+  const scoreDisplay = $('#score');
+  const submitGuessBtn = $('#submitGuessButton');
+  const playAgainBtn = $('#playAgainButton');
+  const hintDisplay = $('#hint');
+
+  let svgArr = [];
+  let category = "";
+  let drawingCount = 0;
+  let playerScore = 0;
+  let continueGame = true;
+  let allDrawingsLoaded = false;
+
+  socket.on('serverSendRandomCategoryName', (cat) => {
+    category = cat;
+    hintDisplay.text(`Hint: ${category.replace(/\S/g, "-")}`);
+    socket.emit('clientRequestCountFromCategory', {
+      category: category,
+      count: drawingDivs.length
+    });
+  });
+
+  socket.on('serverSendDrawing', (drawingData) => {
+    const { word, svg } = drawingData;
+    svgArr.push(svg);
+    $(drawingDivs[drawingCount]).html(svgArr[drawingCount]);
+    drawingCount++;
+    allDrawingsLoaded = drawingCount === drawingDivs.length;
+  });
+
+  input.keypress(function (e) {
+    let key = e.which;
+  
+    if (key == 13) { // hitting enter key
+      submitGuessBtn.click();
+    }
+  });
+
+  return {
+    fillDrawingDivs: function () {
+      svgArr = [];
+      category = "";
+      drawingCount = 0;
+      allDrawingsLoaded = false;
+
+      socket.emit('clientRequestRandomCategoryName', drawingDivs.length);
+    },
+    setSubmitButtonClickEvent: function (clickEvent) {
+      submitGuessBtn.click(clickEvent);
+    },
+    setPlayAgainButtonClickEvent: function (clickEvent) {
+      playAgainBtn.click(clickEvent);
+    },
+    displayScore: function () {
+      scoreDisplay.text(`Score: ${playerScore}`);
+    },
+    incrementScore: function () {
+      ++playerScore;
+    },
+    resetScore: function () {
+      playerScore = 0;
+    },
+    getUserInput: function () {
+      return input.val().trim().toLowerCase();
+    },
+    resetUserInput: function () {
+      input.val('');
+    },
+    getCategory: function () {
+      return category.toLowerCase();
+    },
+    willContinueGame: function () {
+      return continueGame;
+    },
+    stopGame: function () {
+      continueGame = false;
+    },
+    restartGame: function () {
+      continueGame = true;
+    },
+    isAllDrawingsLoaded: function () {
+      return allDrawingsLoaded;
+    },
+    updateHighScore: function () {
+      $.ajax({
+        type: 'PUT',
+        url: `${window.location.pathname}/${playerScore}`,
+        success: (response) => {
+          console.log('Score successfully updated');
+        },
+        error: (jqXHR, textStatus, errorThrown) => {
+          console.log(`Error: ${textStatus} - ${errorThrown}`);
+        }
+      });
+    },
+    displayGameOverScreen: function () {
+      alert(`Game over!\nThe word was "${category}".\n\nScore: ${playerScore}\n\nClick "Play Again" to start a new game!`);
+    }
+  }
 })();
--- public/scripts/standard.js
+++ public/scripts/standard.js
@@ -1,98 +1,34 @@
-const socket = io.connect(window.location.origin);
-
-const drawingDivs = $(".drawing");
-const input = $('#wordInput');
-const scoreDisplay = $('#score');
-const submitGuessBtn = $('#submitGuessButton');
-const playAgainBtn = $('#playAgainButton');
-const hintDisplay = $('#hint');
-
-let svgArr = [];
-let category = "";
-let drawingCount = 0;
-let playerScore = 0;
-let continueGame = true;
-let allDrawingsLoaded = false;
-
-socket.on('serverSendRandomCategoryName', (cat) => {
-  category = cat;
-  hintDisplay.text(`Hint: ${category.replace(/\S/g, "-")}`);
-  socket.emit('clientRequestCountFromCategory', {
-    category: category,
-    count: drawingDivs.length
-  });
-});
-
-socket.on('serverSendDrawing', (drawingData) => {
-  const { word, svg } = drawingData;
-  svgArr.push(svg);
-  $(drawingDivs[drawingCount]).html(svgArr[drawingCount]);
-  drawingCount++;
-  allDrawingsLoaded = drawingCount === drawingDivs.length;
-});
-
-function fillDrawingDivs() {
-  svgArr = [];
-  category = "";
-  drawingCount = 0;
-  allDrawingsLoaded = false;
-
-  socket.emit('clientRequestRandomCategoryName', drawingDivs.length);
-}
-
 $(document).ready(() => {
-  scoreDisplay.text(`Score: ${playerScore}`);
-  fillDrawingDivs();
+  gameEngine.displayScore();
+  gameEngine.fillDrawingDivs();
 });
 
-submitGuessBtn.click(() => {
-  const playerGuess = input.val().trim().toLowerCase();
+gameEngine.setSubmitButtonClickEvent(() => {
+  const playerGuess = gameEngine.getUserInput();
 
-  if (continueGame && playerGuess) {
-    const answer = category.toLowerCase();
+  if (gameEngine.willContinueGame() && playerGuess) {
+    console.log('here');
+    const answer = gameEngine.getCategory();
 
     if (playerGuess === answer) {
-      scoreDisplay.text(`Score: ${++playerScore}`);
-      input.val('');
-
-      fillDrawingDivs();
+      gameEngine.incrementScore()
+      gameEngine.displayScore();
+      gameEngine.resetUserInput();
+      gameEngine.fillDrawingDivs();
     } else {
-      continueGame = false;
-
-      $.ajax({
-        type: 'PUT',
-        url: `${window.location.pathname}/${playerScore}`,
-        success: (response) => {
-          console.log('Score successfully updated');
-        },
-        error: (jqXHR, textStatus, errorThrown) => {
-          console.log(`Error: ${textStatus} - ${errorThrown}`);
-        }
-      });
-
-      alert(`Game over!\nThe word was "${category}".\n\nScore: ${playerScore}\n\nClick "Play Again" to start a new game!`);
+      gameEngine.stopGame();
+      gameEngine.updateHighScore();
+      gameEngine.displayGameOverScreen();
     }
   }
 });
 
-input.keypress(function (e) {
-  let key = e.which;
-
-  if (key == 13) { // hitting enter key
-    submitGuessBtn.click();
-  }
-});
-
-playAgainBtn.click(() => {
-  if (allDrawingsLoaded) {
-    console.log('play again clicked')
-    continueGame = true;
-
-    playerScore = 0;
-
-    scoreDisplay.text(`Score: ${playerScore}`);
-    input.val('');
-
-    fillDrawingDivs();
+gameEngine.setPlayAgainButtonClickEvent(() => {
+  if (gameEngine.isAllDrawingsLoaded()) {
+    gameEngine.restartGame();
+    gameEngine.resetScore();
+    gameEngine.displayScore();
+    gameEngine.resetUserInput();
+    gameEngine.fillDrawingDivs();
   }
 });
