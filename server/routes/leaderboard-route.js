ca105cf58df46b3552aae142aa40a546e0f8c030
Leon Franco <leonard.k.franco@gmail.com>
2019-11-29 18:03:34 -0800

    Refactors leaderboard-route.js

--- server/routes/leaderboard-route.js
+++ server/routes/leaderboard-route.js
@@ -4,7 +4,7 @@ leaderboardRoute.get('/', async (req, res) => {
   console.log('Landed on leaderboard page');
 
   try {
-    const personalScoreQuery = await req.pool.query(`
+    const personalScoreQuery = req.pool.query(`
       SELECT username,standard,odd_one_out,timed
       FROM Users
       WHERE username='${req.session.user}'
@@ -12,19 +12,19 @@ leaderboardRoute.get('/', async (req, res) => {
 
     const maxNumOfScores = 10;
 
-    const topTenStandardScoresQuery = await req.pool.query(`
+    const topTenStandardScoresQuery = req.pool.query(`
       SELECT username,standard
       FROM Users
       ORDER BY standard DESC
       LIMIT ${maxNumOfScores}
     `);
-    const topTenOddOneOutScoresQuery = await req.pool.query(`
+    const topTenOddOneOutScoresQuery = req.pool.query(`
       SELECT username,odd_one_out
       FROM Users
       ORDER BY odd_one_out DESC
       LIMIT ${maxNumOfScores}
     `);
-    const topTenTimedScoresQuery = await req.pool.query(`
+    const topTenTimedScoresQuery = req.pool.query(`
       SELECT username,timed
       FROM Users
       ORDER BY timed DESC
@@ -32,10 +32,10 @@ leaderboardRoute.get('/', async (req, res) => {
     `);
 
     const scores = {
-      personalScore: personalScoreQuery.rows[0],
-      topTenStandardScores: topTenStandardScoresQuery.rows,
-      topTenOddOneOutScores: topTenOddOneOutScoresQuery.rows,
-      topTenTimedScores: topTenTimedScoresQuery.rows
+      personalScore: (await personalScoreQuery).rows[0],
+      topTenStandardScores: (await topTenStandardScoresQuery).rows,
+      topTenOddOneOutScores: (await topTenOddOneOutScoresQuery).rows,
+      topTenTimedScores: (await topTenTimedScoresQuery).rows
     }
 
     res.render('pages/leaderboard', scores);
