42f70665325e53525ceec5533d2d93631c1162b1
Leon Franco <leonard.k.franco@gmail.com>
2019-11-29 16:20:48 -0800

    Fixes weird code duplication in login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -9,7 +9,7 @@ $(selector).submit((event) => {
       url: '/login',
       data: $(selector).serialize(),
       success: (response) => {
-        window.location.href = window.location.href = `${window.location.origin}/main-menu`;
+        window.location.href = `${window.location.origin}/main-menu`;
       },
       error: (jqXHR, textStatus, errorThrown) => {
         console.log(`Error: ${textStatus} - ${errorThrown}`);
