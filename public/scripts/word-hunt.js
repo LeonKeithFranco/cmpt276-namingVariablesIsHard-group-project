48b02ecdabe8bc7823b036902cf6b8d96f322f96
Eric Kozak <ericthehumanstudent@gmail.com>
2019-12-02 01:21:55 -0800

    Word hunt fills divs with random drawings from different categories

--- public/scripts/word-hunt.js
+++ public/scripts/word-hunt.js
@@ -1,7 +1,56 @@
+const socket = io.connect(window.location.origin);
+
+const drawingDivs = $(".drawing");
+
+let svgArr = [];
+let categoryArr = [];
+let category = "";
+let drawingCount = 0;
+let allDrawingsLoaded = false;
+
 $(document).ready(() => {
   gameEngine.displayScore();
-  gameEngine.fillDrawingDivs();
+  fillDrawingDivs();
 });
+
+function fillDrawingDivs() {
+  svgArr = [];
+  categoryArr = [];
+  category = "";
+  drawingCount = 0;
+  allDrawingsLoaded = false;
+
+  socket.emit('clientRequestRandomCategoryNames', drawingDivs.length);
+}
+
+socket.on('serverSendRandomCategoryName', (cat) => {
+  socket.emit('clientRequestFromCategory', cat);
+});
+
+socket.on('serverSendDrawing', (drawingData) => {
+  const { word, svg } = drawingData;
+
+  // assert.isNotEmpty(word);
+  // assert.isString(word);
+
+  // assert.isNotEmpty(svg);
+  // assert.isString(svg);
+  // assert.match(svg, /svg/);
+
+  svgArr.push(svg);
+  categoryArr.push(word);
+  $(drawingDivs[drawingCount]).html(svgArr[drawingCount]);
+  drawingCount++;
+  allDrawingsLoaded = drawingCount === drawingDivs.length;
+
+  if(allDrawingsLoaded) {
+    setCorrectCategory();
+  }
+});
+
+function setCorrectCategory() {
+
+}
 
 gameEngine.setSubmitButtonClickEvent(() => {
   const playerGuess = gameEngine.getUserInput();
@@ -28,11 +77,11 @@ gameEngine.setSubmitButtonClickEvent(() => {
 });
 
 gameEngine.setPlayAgainButtonClickEvent(() => {
-  if (gameEngine.isAllDrawingsLoaded()) {
+  if (allDrawingsLoaded) {
     gameEngine.restartGame();
     gameEngine.resetScore();
     gameEngine.displayScore();
     gameEngine.resetUserInput();
-    gameEngine.fillDrawingDivs();
+    fillDrawingDivs();
   }
 });
