c2f7b1eda42247a3cd5c0c755063f4c427e2a15c
Eric Kozak <ericthehumanstudent@gmail.com>
2019-12-02 00:54:16 -0800

    Changed indentation of word-hunt.js to make it consistent with other js

--- public/scripts/word-hunt.js
+++ public/scripts/word-hunt.js
@@ -1,38 +1,38 @@
 $(document).ready(() => {
-    gameEngine.displayScore();
-    gameEngine.fillDrawingDivs();
+  gameEngine.displayScore();
+  gameEngine.fillDrawingDivs();
 });
 
 gameEngine.setSubmitButtonClickEvent(() => {
-    const playerGuess = gameEngine.getUserInput();
+  const playerGuess = gameEngine.getUserInput();
 
-    if (gameEngine.willContinueGame() && playerGuess) {
-        const answer = gameEngine.getCategory();
+  if (gameEngine.willContinueGame() && playerGuess) {
+    const answer = gameEngine.getCategory();
 
-        // assert.isString(answer);
-        // assert.isNotEmpty(answer);
+    // assert.isString(answer);
+    // assert.isNotEmpty(answer);
 
-        if (playerGuess === answer) {
-            gameEngine.incrementScore()
-            gameEngine.displayScore();
-            gameEngine.playRight();
-            gameEngine.resetUserInput();
-            gameEngine.fillDrawingDivs();
-        } else {
-            gameEngine.playWrong();
-            gameEngine.stopGame();
-            gameEngine.updateHighScore();
-            gameEngine.displayGameOverScreen();
-        }
+    if (playerGuess === answer) {
+      gameEngine.incrementScore()
+      gameEngine.displayScore();
+      gameEngine.playRight();
+      gameEngine.resetUserInput();
+      gameEngine.fillDrawingDivs();
+    } else {
+      gameEngine.playWrong();
+      gameEngine.stopGame();
+      gameEngine.updateHighScore();
+      gameEngine.displayGameOverScreen();
     }
+  }
 });
 
 gameEngine.setPlayAgainButtonClickEvent(() => {
-    if (gameEngine.isAllDrawingsLoaded()) {
-        gameEngine.restartGame();
-        gameEngine.resetScore();
-        gameEngine.displayScore();
-        gameEngine.resetUserInput();
-        gameEngine.fillDrawingDivs();
-    }
+  if (gameEngine.isAllDrawingsLoaded()) {
+    gameEngine.restartGame();
+    gameEngine.resetScore();
+    gameEngine.displayScore();
+    gameEngine.resetUserInput();
+    gameEngine.fillDrawingDivs();
+  }
 });
