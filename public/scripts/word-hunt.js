cef21447ebf131d9d36829e6cd73f6e1e0c9a563
Eric Kozak <ericthehumanstudent@gmail.com>
2019-12-02 01:45:15 -0800

    Adds basic single-round test functionality to word hunt

--- public/scripts/word-hunt.js
+++ public/scripts/word-hunt.js
@@ -1,10 +1,12 @@
 const socket = io.connect(window.location.origin);
 
 const drawingDivs = $(".drawing");
+const categoryDiv = $("#category");
 
 let svgArr = [];
 let categoryArr = [];
 let category = "";
+let correctIndex;
 let drawingCount = 0;
 let allDrawingsLoaded = false;
 
@@ -18,6 +20,7 @@ function fillDrawingDivs() {
   categoryArr = [];
   category = "";
   drawingCount = 0;
+  continueGame = true;
   allDrawingsLoaded = false;
 
   socket.emit('clientRequestRandomCategoryNames', drawingDivs.length);
@@ -49,5 +52,32 @@ socket.on('serverSendDrawing', (drawingData) => {
 });
 
 function setCorrectCategory() {
+  correctIndex = randomRange(drawingDivs.length);
+  category = categoryArr[correctIndex];
 
+  categoryDiv.text(`Category: ${category}`);
+
+  for (let i = 0; i < svgArr.length; i++) {
+    $(drawingDivs[i]).html(svgArr[i]);
+    $(drawingDivs[i]).on('click', function () {
+      select(i);
+    });
+  }
+}
+
+function select(index) {
+  if (continueGame && allDrawingsLoaded) {
+
+    if (index === correctIndex) {
+      alert("correct");
+    } else {
+      alert("incorrect");
+    }
+  }
+}
+
+function randomRange(upperbound) {
+  // assert.isNumber(upperbound);
+
+  return Math.floor(Math.random() * upperbound);
 }
