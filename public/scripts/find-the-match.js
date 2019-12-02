7e5315abcc41eedd36f0985264421628c2e8387b
Eric Kozak <ericthehumanstudent@gmail.com>
2019-12-02 00:14:04 -0800

    Find-the-match mode initialized using standard mode for starting code

---
+++ public/scripts/find-the-match.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+    gameEngine.displayScore();
+    gameEngine.fillDrawingDivs();
+});
+
+gameEngine.setSubmitButtonClickEvent(() => {
+    const playerGuess = gameEngine.getUserInput();
+
+    if (gameEngine.willContinueGame() && playerGuess) {
+        const answer = gameEngine.getCategory();
+
+        // assert.isString(answer);
+        // assert.isNotEmpty(answer);
+
+        if (playerGuess === answer) {
+            gameEngine.incrementScore()
+            gameEngine.displayScore();
+            gameEngine.playRight();
+            gameEngine.resetUserInput();
+            gameEngine.fillDrawingDivs();
+        } else {
+            gameEngine.playWrong();
+            gameEngine.stopGame();
+            gameEngine.updateHighScore();
+            gameEngine.displayGameOverScreen();
+        }
+    }
+});
+
+gameEngine.setPlayAgainButtonClickEvent(() => {
+    if (gameEngine.isAllDrawingsLoaded()) {
+        gameEngine.restartGame();
+        gameEngine.resetScore();
+        gameEngine.displayScore();
+        gameEngine.resetUserInput();
+        gameEngine.fillDrawingDivs();
+    }
+});
